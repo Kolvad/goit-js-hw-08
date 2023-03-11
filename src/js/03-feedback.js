868e657d55835114ce6ee53a15f8dec6050b90f2
Kolvad <vskolesnik@gmail.com>
2023-03-11 16:43:19 +0200

    hw8 task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const emailInput = formRef.querySelector('[name="email"]');
+const messageInput = formRef.querySelector('[name="message"]');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+formRef.addEventListener(
+  'input',
+  throttle(() => {
+    const formData = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }, 500)
+);
+
+const savedFormData = localStorage.getItem(STORAGE_KEY);
+
+if (savedFormData) {
+  const formData = JSON.parse(savedFormData);
+  emailInput.value = formData.email;
+  messageInput.value = formData.message;
+} else {
+  emailInput.value = '';
+  messageInput.value = '';
+}
+
+formRef.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  emailInput.value = '';
+  messageInput.value = '';
+
+  console.log(formData);
+});
