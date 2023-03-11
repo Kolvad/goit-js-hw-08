9e949efa0a600e72f9675532b97235f6957a9796
Kolvad <vskolesnik@gmail.com>
2023-03-11 15:02:13 +0200

    hw7 task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const markup = createMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+function createMarkup(galleryItems) {
+  return galleryItems
+    .map(element => {
+      return `
+          <a class="gallery__item" href="${element.original}">
+            <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+          </a>
+      `;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY) || 0);
